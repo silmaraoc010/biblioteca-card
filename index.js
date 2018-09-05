63cc2d54b706a37e97cc2dac1d0e7866ed65d2b6
silmaraoc010 <silmara_a7x@hotmail.com>
2018-09-04 23:59:50 -0300

    testes com eslintrc

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-function cardValidator (credNum){
-  if (!credNum){
-    throw new Error("Não pode ser vazio");
-  }
-  if (typeof credNum === "string"){
-    throw new Error("Contém string");
-  }
-  if (credNum.toString().length<10){
-    throw new Error("Digitos menor do que o esperado");
-  }
-
-  let numberCard = credNum.toString().split('').reverse();
-
-  let totalSum = 0;
-
-  for (let i = 0; i < numberCard.length; i++){
-    if (i % 2 === 1){
-      let quant = parseInt(numberCard[i])*2;
-      if (quant >=10){
-        totalSum += parseInt(quant / 10) + (quant % 10);
-      }
-    }else {
-      totalSum += parseInt(numberCard[i]);
+function cardValidator(credNum) {
+    if (!credNum) {
+        throw new Error("Não pode ser vazio");
     }
-  }
+    if (typeof credNum === "string") {
+        throw new Error("Contém string");
+    }
+    if (credNum.toString().length < 10) {
+        throw new Error("Digitos menor do que o esperado");
+    }
 
-  if (totalSum %10 === 0){
-    return true;
-  }else{
-    return false;
-  }
- }
+    let numberCard = credNum.toString().split("").reverse();
+    let totalSum = 0;
 
- module.exports.cardValidator = cardValidator;
+    for (let i = 0; i < numberCard.length; i++) {
+        if (i % 2 === 1) {
+            let quant = parseInt(numberCard[i]) * 2;
+            if (quant >= 10) {
+                totalSum += parseInt(quant / 10) + (quant % 10);
+            }
+        } else {
+            totalSum += parseInt(numberCard[i]);
+        }
+    }
+    if (totalSum % 10 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports.cardValidator = cardValidator;
